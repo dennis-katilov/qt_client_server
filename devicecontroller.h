e0347c93bdadf6e1823e32518f8ae35af2044086
Dennis <dennis.katilov@gmail.com>
2024-08-06 14:19:59 +0300

    add tcp socket

--- devicecontroller.h
+++ devicecontroller.h
@@ -2,14 +2,19 @@
 #define DEVICECONTROLLER_H
 
 #include <QObject>
+#include <QTcpSocket>
 
 class devicecontroller : public QObject
 {
     Q_OBJECT
 public:
     explicit devicecontroller(QObject *parent = nullptr);
+    void connectToDevice(QString ip, int port);
 
 signals:
+
+private:
+    QTcpSocket _socket;
 };
 
 #endif // DEVICECONTROLLER_H
