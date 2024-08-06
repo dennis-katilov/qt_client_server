73dd4cccf99c7da371dc2eba624ab8403e38c785
Dennis <dennis.katilov@gmail.com>
2024-08-06 15:45:50 +0300

    some fix to validate ip address

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QStyle>
 #include <QHostAddress>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -17,12 +18,20 @@ MainWindow::~MainWindow()
 void MainWindow::on_IPAddress_textChanged(const QString &arg1)
 {
     // validate ip adress by mask and change background
-    QHostAddress address(arg1);
     QString state = "0";
-    if(QAbstractSocket::IPv4Protocol == address.protocol())
+    if (arg1=="...")
     {
-        state="1";
+        state="";
+    }
+    else
+    {
+        QHostAddress address(arg1);
+        if(QAbstractSocket::IPv4Protocol == address.protocol())
+        {
+            state="1";
+        }
     }
     ui->IPAddress->setProperty("state", state);
+    style()->polish(ui->IPAddress);
 }
 
