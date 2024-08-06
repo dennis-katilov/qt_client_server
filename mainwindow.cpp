8cac8103cd4143be4c03c598eeebf5f27075f818
Dennis <dennis.katilov@gmail.com>
2024-08-06 15:35:37 +0300

    validate ip address

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QHostAddress>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,16 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_IPAddress_textChanged(const QString &arg1)
+{
+    // validate ip adress by mask and change background
+    QHostAddress address(arg1);
+    QString state = "0";
+    if(QAbstractSocket::IPv4Protocol == address.protocol())
+    {
+        state="1";
+    }
+    ui->IPAddress->setProperty("state", state);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_IPAddress_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
 };
